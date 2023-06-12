18bc651b4d3c4cd2e9387a6bee2e7a3c5226d2a7
Floris <floris.defeyter@kuleuven.be>
2023-06-12 21:06:12 +0200

    Add label-based K fold splits

--- kfold.py
+++ kfold.py
@@ -14,15 +14,20 @@ def kfold_split(ds_train, k=5, val_fold=0, seed=15):
     assert val_fold < k
     ds_val = deepcopy(ds_train)
 
-    folds = np.array_split(
-        ds_train.df.sample(frac=1.0, random_state=seed),
-        k
-    )
-    df_val = folds.pop(val_fold)
-    df_train = pd.concat(folds)
-    df_train = df_train.reset_index(drop=True)
-    df_val = df_val.reset_index(drop=True)
+    df_train = ds_train.df
+    df_val = ds_val.df
 
-    ds_train.df = df_train
-    ds_val.df = df_val
+    labels = df_train['label'].unique()
+    np.random.seed(seed)
+    np.random.shuffle(labels)
+    label_folds = np.array_split(labels, k)
+
+    val_labels = label_folds.pop(val_fold)
+    train_labels = np.concatenate(label_folds)
+    df_train = df_train[df_train['label'].isin(train_labels)]
+    df_val = df_val[df_val['label'].isin(val_labels)]
+
+    ds_train.df = df_train.reset_index(drop=True)
+    ds_val.df = df_val.reset_index(drop=True)
+
     return ds_train, ds_val
